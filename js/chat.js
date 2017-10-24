ac4e34a686e06aab11e32c48abd8299a0bc4e687
Yashu Mittal <mittalyashu77@gmail.com>
2017-10-24 20:40:55 +0530

    removed the chat.js file

--- js/chat.js
+++
@@ -1,1 +0,0 @@
-window.$crisp=[];window.CRISP_WEBSITE_ID="fb5e352c-f4c6-45fa-b8ff-f6726c2624fc";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
