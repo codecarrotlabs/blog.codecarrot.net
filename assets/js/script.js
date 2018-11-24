ae18194561eab9fd8af77f2a8557832dde60bfef
Yashu Mittal <mittalyashu77@gmail.com>
2018-11-25 00:37:23 +0530

    Making header more flexible on scrolling
    Header will show on scrolling up and hide on scrolling down and creating a extra space for absolute header, instead of 4px header stripe.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,44 @@
 $(document).ready(function() {
-	$('.nav-toggle').click(function() {
-		$('.nav-list')
-			.toggleClass('is-open');
-	});
-});
+  $('.nav-toggle').click(function() {
+    $('.nav-list').toggleClass('is-open')
+  })
+})
+
+// Hide Header on on scroll down
+var didScroll
+var lastScrollTop = 0
+var delta = 5
+var navbarHeight = $('header').outerHeight()
+
+$(window).scroll(function(event) {
+  didScroll = true
+})
+
+setInterval(function() {
+  if (didScroll) {
+    hasScrolled()
+    didScroll = false
+  }
+}, 250)
+
+function hasScrolled() {
+  var st = $(this).scrollTop()
+  console.log(st);
+  // Make sure they scroll more than delta
+  if (Math.abs(lastScrollTop - st) <= delta)
+    return;
+
+  // If they scrolled down and are past the navbar, add class .nav-up.
+  // This is necessary so you never see what is "behind" the navbar.
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $('header').removeClass('header-show').addClass('header-hide');
+  } else {
+    // Scroll Up
+    if (st + $(window).height() < $(document).height()) {
+      $('header').removeClass('header-hide').addClass('header-show');
+    }
+  }
+
+  lastScrollTop = st
+}
